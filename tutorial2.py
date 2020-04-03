25d1a8c736ac88dc387ec0202d8d66b29a7b998a
l2eop <63106182+l2eop@users.noreply.github.com>
2020-04-03 15:56:51 +0200

    string variables
    combining string variables

---
+++ tutorial2.py
@@ -0,0 +1,18 @@
+my_variable0 = "this is a variable"
+my_variable1 = "this is a second variable"
+my_variable2 = 10.01
+print(my_variable0)
+print(my_variable1)
+print(my_variable2)
+print(my_variable0 + " " + my_variable1)
+my_comb_variable0 = my_variable0 + " " + my_variable1
+print(my_comb_variable0)
+my_variable0 = "this is a changed variable"
+my_comb_variable0 = my_variable0 + " " + my_variable1
+print(my_variable0)
+print(my_variable1)
+print(my_variable2)
+print(my_variable0 + " " + my_variable1)
+print(my_comb_variable0)
+print(type(my_variable0 + my_variable1 + my_comb_variable0))
+print(type(my_variable2))
